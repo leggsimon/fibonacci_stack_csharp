e5c04f38e3163de6eb520537ec8c093794fdeff9
Simon Legg <leggsimon@gmail.com>
2015-10-05 16:27:50 +0100

    First commit

---
+++ fibonacciStack/FibonacciStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fibonacciStack
+{
+	public class FibonacciStack
+	{
+		public Stack<int> GetSequence (int maxValue)
+		{
+			var fibonacciStack = new Stack<int>();
+
+			fibonacciStack.Push (0);
+			fibonacciStack.Push (1);
+
+			BuildSequenceUpToValue (fibonacciStack, maxValue);
+
+			return fibonacciStack;
+		}
+
+		private Stack<int> BuildSequenceUpToValue(Stack<int> stack, int maxValue)
+		{
+			for (int i = 0; i < maxValue - 2; i++)
+			{
+				AddNextNumber (stack);
+				if (stack.Peek() > maxValue)
+				{
+					stack.Pop ();
+					break;
+				}
+			}
+			return stack;
+		} 
+
+		private Stack<int> AddNextNumber(Stack<int> stack)
+		{
+			var lastValue = stack.Pop ();
+			var secondToLastValue = stack.Pop ();
+
+			stack.Push (secondToLastValue);
+			stack.Push (lastValue);
+			stack.Push (secondToLastValue + lastValue);
+
+			return stack;
+		}
+	}
+}
+
---
+++ fibonacciStack/Test.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace fibonacciStack
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void FibonacciStack_ShouldReturnFirst6Integers ()
+		{
+			var fibonacci = new FibonacciStack ();
+			var value = 6;
+			var expected = 5;
+
+			var sequence = fibonacci.GetSequence (value);
+			var actual = sequence.Pop ();
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test ()]
+		public void FibonacciStack_ShouldReturnUpToValue ()
+		{
+			var fibonacci = new FibonacciStack ();
+			var value = 15;
+			var expected = 13;
+
+			var sequence = fibonacci.GetSequence (value);
+			var actual = sequence.Pop ();
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
+
